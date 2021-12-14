cdce8d4da7ef977963e511c02f0d00d72196ec5d
Dipperip <93734009+Dipperip@users.noreply.github.com>
2021-12-14 10:12:40 +0700

    splash anim

--- app/src/main/java/com/example/b4u/SplashActivity.java
+++ app/src/main/java/com/example/b4u/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.b4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
     }
 }
