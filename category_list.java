6df8d0501a7c2fe36684080b319ab8e59a7c2780
19520909 <93769577+19520909@users.noreply.github.com>
2021-12-19 17:32:09 +0700

    Add files via upload

--- category_list.java
+++ category_list.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,10 +13,21 @@ public class category_list extends AppCompatActivity {
     ImageView careimg,careback,carecart,careimg2,careimg3,careimg4;
     int careimage,careimage2,careimage3,careimage4;
     TextView careproductname;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cate_1);
+        carecart = findViewById(R.id.carecart);
+        careback = findViewById(R.id.careback);
+        careback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iNewActivity = new Intent(category_list.this,MainActivity.class);
+                startActivity((iNewActivity));
+                finish();
+            }
+        });
         Intent i = getIntent();
         carename = i.getStringExtra("carename");
         careimage = i.getIntExtra("careimage",R.drawable.product_1);
@@ -28,5 +40,8 @@ public class category_list extends AppCompatActivity {
         careimg4 = findViewById(R.id.careimageProduct4);
         careimg.setImageResource(careimage);
         careimg.setImageResource(careimage2);
+
+
     }
+
 }
